e98b91e7763d094725362278ee2535d2bf741ff9
unknown <thiebautp@gmail.com>
2019-12-30 07:50:24 +0100

    Page prestations (début)

---
+++ application/controllers/prestations/PrestationsController.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class PrestationsController
+{
+    public function httpGetMethod(Http $http, array $queryFields)
+    {
+
+      
+
+    }
+
+    public function httpPostMethod(Http $http, array $formFields)
+    {
+
+
+
+    }
+}
+
+ ?>
