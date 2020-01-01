f6aaa582f5347672923dbabba893e56d3c20c33d
unknown <thiebautp@gmail.com>
2020-01-01 20:02:00 +0100

    requete chambre dispo

--- application/controllers/reservation/ReservationController.class.php
+++ application/controllers/reservation/ReservationController.class.php
@@ -2,25 +2,30 @@
 
 class ReservationController
 {
-    public function httpGetMethod(Http $http, array $queryFields)
-    {
+  public function httpGetMethod(Http $http, array $queryFields)
+  {
 
 
 
-    }
+  }
 
-    public function httpPostMethod(Http $http, array $formFields)
-    {
+  public function httpPostMethod(Http $http, array $formFields)
+  {
+    $reservationModel = new ReservationModel();
 
-      var_dump($_POST);
-      $date = new DateTime();
-      $dateToday = $date->format('d-m-Y');
-      var_dump ($dateToday);
-      $dateArrival = $_POST["datePickerArrival"];
-      $dateDeparture = $_POST["datePickerDeparture"];
-      var_dump($dateArrival);
-      var_dump($dateDeparture);
-    }
+    $dateArrival = $_POST["datePickerArrival"];
+    $dateDeparture = $_POST["datePickerDeparture"];
+    // var_dump($dateArrival);
+    // var_dump($dateDeparture);
+    $dateBeginning = $reservationModel->goToEnglishFormat($dateArrival);
+    var_dump($dateBeginning);
+    $dateEnd = $reservationModel->goToEnglishFormat($dateDeparture);
+    var_dump($dateEnd);
+
+    // var_dump($_POST);
+    $reservation = $reservationModel->CheckRoomAvailability($dateBeginning, $dateEnd, $_POST);
+    var_dump($reservation);
+  }
 }
 
- ?>
+?>
--- application/models/ReservationModel.class.php
+++ application/models/ReservationModel.class.php
@@ -2,9 +2,19 @@
 
 class ReservationModel {
 
-  public function CheckRoomAvailability() {
+  public function goToEnglishFormat($dateFR) {
+    $dateTab = explode("/", $dateFR);
+    $date = $dateTab[2]."-".$dateTab[1]."-".$dateTab[0];
+    // var_dump($date);
+    return $date;
+  }
+
+  public function CheckRoomAvailability($dateBeginning, $dateEnd, $_post) {
     $database = new Database();
-    // $sql = "SELECT "
+    $sql = "SELECT ReservationNumber FROM reservations WHERE STR_TO_DATE(?,'%Y-%m-%d') BETWEEN ArrivalDate AND DepartureDate OR STR_TO_DATE(?,'%Y-%m-%d') BETWEEN ArrivalDate AND DepartureDate AND RoomId =?";
+    $array = [$dateBeginning, $dateEnd, $_post["roomId"]];
+    $reservation = $database->query($sql, $array);
+    return $reservation;
   }
 
 }
