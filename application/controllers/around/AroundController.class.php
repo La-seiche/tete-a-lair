580b4de97b98155f23c0084491b1f5d2e5807e16
unknown <thiebautp@gmail.com>
2020-06-16 01:07:32 +0200

    mise à jour home + création pages

---
+++ application/controllers/around/AroundController.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class AroundController
+{
+    public function httpGetMethod(Http $http, array $queryFields)
+    {
+
+
+
+    }
+
+    public function httpPostMethod(Http $http, array $formFields)
+    {
+
+
+
+    }
+}
+
+ ?>
