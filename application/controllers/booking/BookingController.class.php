bca4f6cf33d1d5b226e09d846fdb59af721aaed3
La-seiche <thiebautp@gmail.com>
2021-03-18 20:19:39 +0100

    affichage résultat booking

--- application/controllers/booking/BookingController.class.php
+++ application/controllers/booking/BookingController.class.php
@@ -23,20 +23,25 @@ class BookingController
       $dateDeparture = $_POST["dateDeparture"];
       // var_dump($dateArrival);
       // var_dump($dateDeparture);
+      $dateArrival_Fr = date("d-m-y", strtotime($dateArrival));
+      // var_dump($dateArrival_Fr);
+      $dateDeparture_Fr = date("d-m-y", strtotime($dateDeparture));
 
       $reservation = $reservationModel->checkRoomAvailability($dateArrival, $dateDeparture, $_POST["roomId"]);
       // var_dump($reservation);
 
       if (empty($reservation)) {
         // var_dump("Chambre dispo");
+        $available = true;
         $calendars = $reservationModel->getRoomCalendars($dateArrival, $dateDeparture, $_POST["roomId"]);
         // var_dump($calendars);
         $bookingDetails = $reservationModel->getBookingDetails($dateArrival, $dateDeparture, $_POST["roomId"], $calendars);
         // var_dump($bookingDetails);
       }
       else {
-        $error = "Désolé cette chambre n'est pas disponible à cette période !";
+        // $error = "Désolé cette chambre n'est pas disponible à cette période !";
         // var_dump($error);
+        $available = false;
       }
 
       $roomsAvailable = $reservationModel->filterRooms($_POST);
@@ -59,7 +64,14 @@ class BookingController
 
         // var_dump($roomsAvailableBookingDetails);
 
-        return ["pictures" => $pictures, "dateArrival" => $dateArrival, "dateDeparture" => $dateDeparture, "bookingDetails" => $bookingDetails, "roomsAvailableBookingDetails" => $roomsAvailableBookingDetails];
+        if ($available)
+        {
+          return ["pictures" => $pictures, "dateArrival" => $dateArrival_Fr, "dateDeparture" => $dateDeparture_Fr, "bookingDetails" => $bookingDetails, "roomsAvailableBookingDetails" => $roomsAvailableBookingDetails, "available" => $available];
+        }
+        else
+        {
+          return ["dateArrival" => $dateArrival_Fr, "dateDeparture" => $dateDeparture_Fr, "roomsAvailableBookingDetails" => $roomsAvailableBookingDetails, "available" => $available];
+        }
       }
 
 }
